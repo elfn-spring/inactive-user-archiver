4b1e91159af3d20bf35e9c9ddbefa7b68cf914a2
Elfn <elimanefofana17@gmail.com>
2025-04-05 04:04:34 +0200

    Ajout des configurations et des données pour la BD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, email, last_login_date)
+VALUES ('alice', 'alice@example.com', DATEADD('DAY', -100, CURRENT_DATE())),
+       ('bob', 'bob@example.com', DATEADD('DAY', -95, CURRENT_DATE())),
+       ('carla', 'carla@example.com', DATEADD('DAY', -20, CURRENT_DATE())),
+       ('david', 'david@example.com', DATEADD('DAY', -200, CURRENT_DATE())),
+       ('emilie', 'emilie@example.com', DATEADD('DAY', -5, CURRENT_DATE()));
