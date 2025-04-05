c27c7e4d5594577ad27b825a20497164ddde55bb
Elfn <elimanefofana17@gmail.com>
2025-04-05 04:05:28 +0200

    Ajout des configurations et des données pour la BD

---
+++ src/main/java/com/elfn/inactiveuserarchiver/InactiveUserArchiverApplication.java
@@ -0,0 +1,13 @@
+package com.elfn.inactiveuserarchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InactiveUserArchiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InactiveUserArchiverApplication.class, args);
+    }
+
+}
